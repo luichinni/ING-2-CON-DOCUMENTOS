563ea67dba90f3a707a2ba26f65b5496b381f91c
Mauguerzz <mauriaplafi@gmail.com>
2024-05-19 15:18:04 -0300

    instalacion de axios y se empieza a modificar publis

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -12,16 +12,9 @@ export function NavBar(){
                     className="botonNavBar"> 
                     Publicaciones
                 </Link>
-                {
-                //<Link to={"../IniciarSesion.js"} className="botonHader"> Iniciar sesión </Link>
-                }
             </li>
             <li className='button-NavBar'>
                 <a href="https://caritas.org.ar/quienes-somos/" className="botonNavBar">¿Quiénes somos?</a>
-                {//<a href={"https://caritas.org.ar/quienes-somos/"} className="botonHeader"> ¿Quiénes somos? </a>
-                // <Link to={"../Registrarse.js"} className="botonHeader"> Regístrarse </Link>
-                //<Link to={"../IniciarSesion.js"} className="botonHader"> Iniciar sesión </Link>
-                }
             </li>
             <li className='button-NavBar'>
                 <Link 
--- front/src/pages/publicaciones/ListarPublis.js
+++ front/src/pages/publicaciones/ListarPublis.js
@@ -1,13 +1,48 @@
+import axios from 'axios';
 import Tarjeta from './Tarjeta';
 import { ListaDePublicaciones } from './ListaDePublicaciones';
 import '../../HarryStyles/Publicaciones.css';
+import { useEffect, useState } from 'react';
 
 const ListarPublis = () => {
+  const [Publi, setPubli] = useState([]);
+  const [parametros, setParametros] = useState({
+        nombre: "",
+        user: "",
+        categoria: "",
+        estado: "",
+        id: ""
+  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const queryParams = new URLSearchParams(parametros);
+        const url = `https://localhost:8000/public/listarPublicaciones?${queryParams.toString()}`;
+        const response = await axios.get(url);
+
+        if (response.data.length === 0){
+          setError('No hay publicaciones disponibles')
+          return <h1 className="SinPubli">¡No hay publicaciones disponibles en este momento!</h1>;
+         } else {
+          setPubli(response.data);
+          setError('');
+         }
+
+        console.log(response.data);
+      } catch (error) {
+        setError('Ocurrio un error al obtener las publicaciones.');
+        console.log(error);
+      }
+    };
   
-  if (ListaDePublicaciones.length === 0){
-    return <h1 className="SinPubli">¡No hay publicaciones disponibles en este momento!</h1>;
-  }
+    fetchData();
+  }, [parametros]);
   
+const handleParametrosChange = (newParametros) => {
+    setParametros(newParametros);
+};
+
   const filasDePublicaciones = [];
   for (let i = 0; i < ListaDePublicaciones.length; i += 3) {
     filasDePublicaciones.push(ListaDePublicaciones.slice(i, i + 3));
