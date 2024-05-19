c707f42586583ef87202f3db358f67c7cd428694
Luciano <98102676+luichinni@users.noreply.github.com>
2024-05-19 16:19:54 -0300

    update index con todos los archivos de rutas

--- apiBaseDeDatos/public/index.php
+++ apiBaseDeDatos/public/index.php
@@ -13,7 +13,12 @@ $app->addErrorMiddleware(true, true, true);
 
 require __DIR__ . '/../src/routes/usuarios.php';
 require __DIR__ . '/../src/routes/sesionActiva.php';
-
+require __DIR__ . '/../src/routes/centros.php';
+require __DIR__ . '/../src/routes/centroVolun.php';
+require __DIR__ . '/../src/routes/publi_centro.php';
+require __DIR__ . '/../src/routes/cetegorias.php';
+require __DIR__ . '/../src/routes/imagen.php';
+require __DIR__ . '/../src/routes/publicacion.php';
 require __DIR__ . '/../src/routes/middlewares.php';
 
 $app->run();
