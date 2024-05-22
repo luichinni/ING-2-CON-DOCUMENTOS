22e7e0c3275d9e804834e656f6a0eb4c69da39ff
Mauguerzz <mauriaplafi@gmail.com>
2024-05-22 13:12:49 -0300

    listarPublis

--- front/src/components/Filtro.js
+++ front/src/components/Filtro.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../HarryStyles/Filtro.css';
 
 const Filtro = ({ onFiltroSubmit }) => {
   const [categorias, setCategorias] = useState('');
@@ -22,27 +23,27 @@ const Filtro = ({ onFiltroSubmit }) => {
   };
 
   useEffect(() => {
-		const fetchData = async () => {
-		  try {
-			const respon = await axios.get(`http://localhost:8000/public/listarCategorias?id=&nombre=`);
-			setCategorias(procesarcat(respon.data));
-			console.log(respon.data);
-		  } catch (error) {
-			console.error(error);
-		  }
-		};
-		fetchData();
-	  }, []);
-
-    function procesarcat(categorias) {
-      let cateCopy = [];
-      Object.keys(categorias).forEach(function (clave) {
-        if (!isNaN(clave)) {
-        cateCopy[clave] = categorias[clave]
-        }
-      })
-      return cateCopy
+    const fetchData = async () => {
+      try {
+        const respon = await axios.get(`http://localhost:8000/public/listarCategorias?id=&nombre=`);
+        setCategorias(procesarcat(respon.data));
+        console.log(respon.data);
+      } catch (error) {
+        console.error(error);
       }
+    };
+    fetchData();
+  }, []);
+
+  function procesarcat(categorias) {
+    let cateCopy = [];
+    Object.keys(categorias).forEach(function (clave) {
+      if (!isNaN(clave)) {
+        cateCopy[clave] = categorias[clave];
+      }
+    });
+    return cateCopy;
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -50,19 +51,46 @@ const Filtro = ({ onFiltroSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="nombre" value={filtro.nombre} onChange={handleChange} placeholder="Nombre" />
-      <input type="text" name="user" value={filtro.user} onChange={handleChange} placeholder="Usuario" />
-      {/*+<select id="categoria" name="categoria_id" onChange={handleCategoriaChange}>
-          <option value="">Categorias</option>
-          {categorias.map((categoria) => (
-            <option key={categoria.id} value={categoria.id}>
-              {categoria.nombre}
-            </option>
-          ))
-        </select>*/}
-      <input type="text" name="estado" value={filtro.estado} onChange={handleChange} placeholder="Estado" />
-      <button type="submit">Filtrar</button>
+    <form className="filtro-form" onSubmit={handleSubmit}>
+      <input
+        className="filtro-input"
+        type="text"
+        name="nombre"
+        value={filtro.nombre}
+        onChange={handleChange}
+        placeholder="Nombre"
+      /><br/><br/>
+      <input
+        className="filtro-input"
+        type="text"
+        name="user"
+        value={filtro.user}
+        onChange={handleChange}
+        placeholder="Usuario"
+      /><br/><br/>
+      <select
+        className="filtro-input"
+        id="categoria"
+        name="categoria_id"
+        value={filtro.categoria_id}
+        onChange={handleChange}
+      >
+        <option value="">Categorías</option>
+        {categorias.map((categoria) => (
+          <option key={categoria.id} value={categoria.id}>
+            {categoria.nombre}
+          </option>
+        ))}
+      </select><br/><br/>
+      <input
+        className="filtro-input"
+        type="text"
+        name="estado"
+        value={filtro.estado}
+        onChange={handleChange}
+        placeholder="Estado"
+      /><br/><br/>
+      <button className="filtro-button" type="submit">Filtrar</button>
     </form>
   );
 }
