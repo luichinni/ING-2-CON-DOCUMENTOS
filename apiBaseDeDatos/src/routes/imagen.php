d7a9b1667ca46b1da8828f2b9ff9259ed461d2f1
Luciano <lumacias11702@gmail.com>
2024-05-22 13:16:41 -0300

    fotos más grandes, base64 hasta 16MB

--- apiBaseDeDatos/src/routes/imagen.php
+++ apiBaseDeDatos/src/routes/imagen.php
@@ -5,7 +5,7 @@
 require_once __DIR__ . '/../utilities/bdController.php';
 
 $camposImg = [
-    'archivo' => 'varchar',
+    'archivo' => 'mediumtext',
     'publicacion' => 'int',
 /*     'tipo_imagen' => 'varchar', */
     'id' => '?int'
