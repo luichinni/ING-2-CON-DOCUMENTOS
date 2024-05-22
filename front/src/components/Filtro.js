cc57de5b29c938bd338f15104c511edd0d00902b
Mauguerzz <mauriaplafi@gmail.com>
2024-05-22 06:34:03 -0300

    publi detalle y filtros
    saque el filtro mis publis oprque no se usa, modifique el filtro trayendo categorias de la BD y paso publidetalle lo que estaba haciendi al menos, pero nose bien como detectar que imagen es, habia pensado con un localStorage para el id y dsp traer todo filtrando por ese id

--- front/src/components/Filtro.js
+++ front/src/components/Filtro.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filtro = ({ onFiltroSubmit }) => {
+  const [categorias, setCategorias] = useState('');
   const [filtro, setFiltro] = useState({
     nombre: "",
     user: "",
@@ -9,6 +11,8 @@ const Filtro = ({ onFiltroSubmit }) => {
     id: ""
   });
 
+  const handleCategoriaChange = (e) => setCategorias(e.target.value);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFiltro({
@@ -16,6 +20,29 @@ const Filtro = ({ onFiltroSubmit }) => {
       [name]: value
     });
   };
+
+  useEffect(() => {
+		const fetchData = async () => {
+		  try {
+			const respon = await axios.get(`http://localhost:8000/public/listarCategorias?id=&nombre=`);
+			setCategorias(procesarcat(respon.data));
+			console.log(respon.data);
+		  } catch (error) {
+			console.error(error);
+		  }
+		};
+		fetchData();
+	  }, []);
+
+    function procesarcat(categorias) {
+      let cateCopy = [];
+      Object.keys(categorias).forEach(function (clave) {
+        if (!isNaN(clave)) {
+        cateCopy[clave] = categorias[clave]
+        }
+      })
+      return cateCopy
+      }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,7 +53,14 @@ const Filtro = ({ onFiltroSubmit }) => {
     <form onSubmit={handleSubmit}>
       <input type="text" name="nombre" value={filtro.nombre} onChange={handleChange} placeholder="Nombre" />
       <input type="text" name="user" value={filtro.user} onChange={handleChange} placeholder="Usuario" />
-      <input type="text" name="categoria_id" value={filtro.categoria_id} onChange={handleChange} placeholder="Categoría_id" />
+      {/*+<select id="categoria" name="categoria_id" onChange={handleCategoriaChange}>
+          <option value="">Categorias</option>
+          {categorias.map((categoria) => (
+            <option key={categoria.id} value={categoria.id}>
+              {categoria.nombre}
+            </option>
+          ))
+        </select>*/}
       <input type="text" name="estado" value={filtro.estado} onChange={handleChange} placeholder="Estado" />
       <button type="submit">Filtrar</button>
     </form>
