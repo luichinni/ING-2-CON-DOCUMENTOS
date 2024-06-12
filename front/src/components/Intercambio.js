9cafc1620da57e0e801f54018d3e8358e565a5b0
Mauguerzz <mauriaplafi@gmail.com>
2024-06-12 16:19:47 -0300

    Update Intercambio.js

--- front/src/components/Intercambio.js
+++ front/src/components/Intercambio.js
@@ -23,20 +23,23 @@ const Intercambio = ({ id, publicacionOferta, publicacionOfertada, centro, horar
       setError('');
 
       try {
-        console.log(`oferta acepta${ofertaAcepta}`)
-        console.log(`ofertada acepta${ofertadaAcepta}`)
-        console.log(`username: ${username}`)
+        console.log(`oferta acepta ${ofertaAcepta}`);
+        console.log(`ofertada acepta ${ofertadaAcepta}`);
+        console.log(`username: ${username}`);
+        
         const url1 = `http://localhost:8000/public/listarPublicaciones?id=${publicacionOferta}&token=${Token}`;
         const response1 = await axios.get(url1);
         
         if (response1.data) {
           const publicaciones = procesar(response1.data);
           setPubli1(publicaciones);
-          setUserPubli(publicaciones[0]?.user || '');
-          console.log(`userPubli: ${userPubli}`)
+          const userPub = publicaciones[0]?.user || 'nop';
+          setUserPubli(userPub);
+          console.log(`userPubli: ${userPub}`);
         } else {
           setError('No hay publicaciones disponibles.');
           setPubli1([]);
+          setUserPubli('nop');
         }
       } catch (error) {
         setError('No hay publicaciones disponibles.');
@@ -50,11 +53,13 @@ const Intercambio = ({ id, publicacionOferta, publicacionOfertada, centro, horar
         if (response2.data) {
           const publicaciones = procesar(response2.data);
           setPubli2(publicaciones);
-          setUserOferto(publicaciones[0]?.user || '');
-          console.log(`userOferto: ${userOferto}`)
+          const userOfer = publicaciones[0]?.user || 'nop';
+          setUserOferto(userOfer);
+          console.log(`userOferto: ${userOfer}`);
         } else {
           setError('No hay publicaciones disponibles.');
           setPubli2([]);
+          setUserOferto('nop');
         }
       } catch (error) {
         setError('No hay publicaciones disponibles.');
@@ -94,7 +99,7 @@ const Intercambio = ({ id, publicacionOferta, publicacionOfertada, centro, horar
       });
 
       if (respon.data.length === 3) {
-        setError('No se realizo la modificación.');
+        setError('No se realizó la modificación.');
       } else {
         window.location.reload();
       }
@@ -116,7 +121,7 @@ const Intercambio = ({ id, publicacionOferta, publicacionOfertada, centro, horar
       });
 
       if (respon.data.length === 3) {
-        setError('No se realizo la modificación.');
+        setError('No se realizó la modificación.');
       } else {
         window.location.reload();
       }
@@ -132,14 +137,13 @@ const Intercambio = ({ id, publicacionOferta, publicacionOfertada, centro, horar
 
   return (
     <li className="intercambio-item">
-      <br /><br /><br /><br />
       <div className="intercambio-content">
         <div className="publicaciones-container">
           <div className="publicacion">
-            Publicacion
+            Publicación
             {publi1.map(publicacion => (
               <Publicacion
-                key={publicacion.id} // para que no llore react
+                key={publicacion.id}
                 id={publicacion.id}
                 nombre={publicacion.nombre}
                 descripcion={publicacion.descripcion}
@@ -172,6 +176,7 @@ const Intercambio = ({ id, publicacionOferta, publicacionOfertada, centro, horar
           <p><strong>Centro:</strong> {centro}</p>
           <p><strong>Horario:</strong> {horario}</p>
           <p><strong>Estado:</strong> {estado}</p>
+  
           {estado === 'aceptado' || estado === 'pendiente' ? (
             Token === 'tokenAdmin' || Token === 'tokenVolunt' ? (
               <button className="detalle-button" onClick={handleValidarClick}>
@@ -185,21 +190,21 @@ const Intercambio = ({ id, publicacionOferta, publicacionOfertada, centro, horar
                 <button className="detalle-button" onClick={handleRechazadoClick}>
                   Rechazar
                 </button>
-                {((userPubli === username && ofertaAcepta === '0') || (userOferto === username && ofertadaAcepta === '0')) && (
-                  <>
-                  {console.log("entro")}
-                  <button className="detalle-button" onClick={handleAceptadoClick}>
-                    Confirmar
-                  </button>
-                  </>
-                )}
+                {console.log(`Entro a condición de confirmar: ${userPubli} y ${userOferto}`)}
               </>
             )
+          ) : ((userPubli == username && ofertaAcepta == false) || (userOferto == username && ofertadaAcepta == true)) ? (
+            <>
+              {console.log("Entro a condición de confirmar")}
+              <button className="detalle-button" onClick={handleAceptadoClick}>
+                Confirmar
+              </button>
+            </>
           ) : null}
         </div>
       </div>
     </li>
   );
-};
+}  
 
 export default Intercambio;
